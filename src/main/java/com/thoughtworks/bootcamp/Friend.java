9ae0d0980e5da4afa1d9e30e04711932709cf3d7
snehamanojraopalekar <nehapalekar026@gmail.com>
2019-09-01 22:02:05 +0530

    [Neha] Add. setting zero amount for single person

---
+++ src/main/java/com/thoughtworks/bootcamp/Friend.java
@@ -0,0 +1,19 @@
+package com.thoughtworks.bootcamp;
+
+public class Friend {
+
+    String name;
+    double amountToSpend;
+    double amountSpent;
+
+    public Friend(String name, double amountToSpend, double amountSpent) {
+        this.name = name;
+        this.amountToSpend = amountToSpend;
+        this.amountSpent = amountSpent;
+    }
+
+    public double settingAmountToSpend() {
+        return 0.0;
+    }
+}
+
--- src/test/java/com/thoughtworks/bootcamp/FriendTest.java
+++ src/test/java/com/thoughtworks/bootcamp/FriendTest.java
@@ -1,7 +1,14 @@
 package com.thoughtworks.bootcamp;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class FriendTest {
 
-
+    @Test
+    void GivenZeroAmountSpentForSinglePerson_WhenCalculatedAmount_To_Spend_ThenShouldReturnZero()
+    {
+        Friend friend = new Friend("Cool",0.0,0.0);
+        Assertions.assertEquals(0.0,friend.settingAmountToSpend());
+    }
 }
